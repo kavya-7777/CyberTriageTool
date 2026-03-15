d0b5b3be0e355b9a541b70aec3b5f6e02f535e06
kavya-7777 <kavya77.m@gmail.com>
2026-03-15 12:35:59 +0530

    add

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\kavya\Desktop\cyber-triage-tool\.venv"
+set "VIRTUAL_ENV=C:\Users\kavya\Desktop\CyberTriage\cyber-triage-tool\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
